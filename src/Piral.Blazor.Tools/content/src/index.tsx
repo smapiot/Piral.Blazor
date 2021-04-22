c1d0351e6f6f580b216bb548a0a781a542ee9351
Dante De Ruwe <dantederuwe@gmail.com>
2021-04-22 17:44:06 +0200

    Refactor src files
    Mainly renaming codegen exports

--- src/Piral.Blazor.Tools/content/src/index.tsx
+++ src/Piral.Blazor.Tools/content/src/index.tsx
@@ -1,17 +1,10 @@
-import * as React from 'react';
-import { PiletApi } from '**PiralInstance**';
-import {
-    registerDependencies,
-    registerBlazorPages,
-    registerBlazorExtensions,
-    registerBlazorOptions,
-    setupPilet,
-} from './blazor.codegen';
+import { PiletApi } from 'demo-piral-blazor-appshell';
+import * as Blazor from './blazor.codegen';
 
 export function setup(app: PiletApi) {
-    registerDependencies(app);
-    registerBlazorOptions(app);
-    registerBlazorPages(app);
-    registerBlazorExtensions(app);
-    setupPilet(app);
+    Blazor.registerDependencies(app);
+    Blazor.registerOptions(app);
+    Blazor.registerPages(app);
+    Blazor.registerExtensions(app);
+    Blazor.setupPilet(app);
 }
