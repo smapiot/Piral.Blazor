ec9e1a0c1b706e520759bf4589f6f4e014030731
Dante De Ruwe <dantederuwe@gmail.com>
2021-04-22 17:39:40 +0200

    Define the resource path root so static files are prefixed
    This is related to smapiot/piral#378

--- src/Piral.Blazor.Tools/content/src/index.tsx
+++ src/Piral.Blazor.Tools/content/src/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import { PiletApi } from '**PiralInstance**';
-import { registerDependencies, registerBlazorPages, registerBlazorExtensions, setupPilet } from './blazor.codegen';
+import {
+    registerDependencies,
+    registerBlazorPages,
+    registerBlazorExtensions,
+    registerBlazorOptions,
+    setupPilet,
+} from './blazor.codegen';
 
 export function setup(app: PiletApi) {
     registerDependencies(app);
+    registerBlazorOptions(app);
     registerBlazorPages(app);
     registerBlazorExtensions(app);
     setupPilet(app);
