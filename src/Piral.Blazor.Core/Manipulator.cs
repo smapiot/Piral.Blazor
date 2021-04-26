8f6f0441a29393a86de4029d1a0c85e9471e8f7b
Dante De Ruwe <dantederuwe@gmail.com>
2021-04-26 10:12:35 +0200

    Try patching Manipulator
    This may or may not be the cause of breakpoints not triggering

--- src/Piral.Blazor.Core/Manipulator.cs
+++ src/Piral.Blazor.Core/Manipulator.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Concurrent;
+using System.Linq;
 using System.Reflection;
 
 namespace Piral.Blazor.Core
@@ -9,25 +10,43 @@ namespace Piral.Blazor.Core
     public class Manipulator<T>
     {
         private readonly ILogger<T> _logger;
-        private readonly Type _factory;
-        private readonly object _instance;
+        private readonly Type _factoryType;
+        private readonly object _factoryInstance;
         private readonly object _initializers;
 
         public Manipulator(ILogger<T> logger)
         {
             _logger = logger;
-            _factory = typeof(ComponentBase).Assembly.GetType("Microsoft.AspNetCore.Components.ComponentFactory");
-            _instance = _factory.GetField("Instance").GetValue(null);
-            _initializers = _factory.GetField("_cachedInitializers", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(_instance);
+
+            var activatorType = typeof(ComponentBase).Assembly
+                .GetTypes()
+                .Single(t => t.Name.Equals("DefaultComponentActivator"));
+
+            var activatorInstance = activatorType
+                .GetProperty("Instance")
+                .GetValue(null); //static value
+            
+            _factoryType = typeof(ComponentBase).Assembly
+                .GetTypes().Single(t => t.Name.Equals("ComponentFactory"));
+
+            _factoryInstance = _factoryType
+                .GetConstructor(new[] { activatorType })
+                .Invoke(new[] { activatorInstance });
+
+            _initializers = _factoryType
+                .GetField("_cachedInitializers", BindingFlags.Instance | BindingFlags.NonPublic)
+                .GetValue(_factoryInstance);
         }
 
         public void OverrideComponentInitializer(Type componentType, IServiceProvider provider)
         {
             try
             {
-                _factory.GetMethod("InstantiateComponent").Invoke(_instance, new object[] { provider, componentType });
+                _factoryType.GetMethod("InstantiateComponent")
+                    .Invoke(_factoryInstance, new object[] { provider, componentType });
                 var converters = _initializers as ConcurrentDictionary<Type, Action<IServiceProvider, IComponent>>;
-                converters.AddOrUpdate(componentType, _ => null, (_, initializer) => (_, comp) => initializer(provider, comp));
+                converters.AddOrUpdate(componentType, _ => null,
+                    (_, initializer) => (_, comp) => initializer(provider, comp));
             }
             catch (Exception ex)
             {
