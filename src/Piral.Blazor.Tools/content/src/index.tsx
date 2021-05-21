8f1c74e4ed00c5e24b359d08dcc57532072532df
Dante De Ruwe <dantederuwe@gmail.com>
2021-05-21 18:01:10 +0200

    Enable support for piral-blazor in the pilet + add extra checks

--- src/Piral.Blazor.Tools/content/src/index.tsx
+++ src/Piral.Blazor.Tools/content/src/index.tsx
@@ -3,6 +3,10 @@ import * as Blazor from './blazor.codegen';
 import './**BlazorProjectName**.styles.css';
 
 export function setup(app: PiletApi) {
+    const pb = Blazor.getPiralBlazorApi(app);
+    app.fromBlazor = pb.fromBlazor;
+    app.defineBlazorReferences = pb.defineBlazorReferences;
+    
     Blazor.registerDependencies(app);
     Blazor.registerOptions(app);
     Blazor.registerPages(app);
