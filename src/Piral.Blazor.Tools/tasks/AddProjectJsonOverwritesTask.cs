896da02bf7b907bd68a8bf4b078f6ba5ee54b11d
Lukas Eiter <lukas.eiter@casablanca.at>
2022-02-11 13:36:22 +0100

    add package json overwrites task and fix version replacement

---
+++ src/Piral.Blazor.Tools/tasks/AddProjectJsonOverwritesTask.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace Piral.Blazor.Tools.Tasks
+{
+    public class AddProjectJsonOverwritesTask : Task
+    {
+        [Required]
+        public string PackageJsonPath { get; set; }
+
+        [Required]
+        public string OverwritesPath { get; set; }
+
+        public override bool Execute()
+        {
+            //System.Diagnostics.Debugger.Launch(); 
+            try
+            { 
+                if (!File.Exists(PackageJsonPath)) {
+                    return false;
+                }
+                if (!File.Exists(OverwritesPath)) 
+                {
+                    return true;
+                }
+                var result = new JObject();
+
+                var packageJson = JObject.Parse(File.ReadAllText(PackageJsonPath)); 
+                var overwritesJson = JObject.Parse(File.ReadAllText(OverwritesPath)); 
+
+                result.Merge(packageJson); 
+                result.Merge(overwritesJson); 
+
+                File.WriteAllText(PackageJsonPath, JsonConvert.SerializeObject(result, Formatting.Indented));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true; 
+        }
+    }
+}
---
+++ src/Piral.Blazor.Tools/tasks/SetProjectJsonVersionTask.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.IO;
+
+namespace Piral.Blazor.Tools.Tasks
+{
+    public class SetProjectJsonVersionTask : Task
+    {
+        [Required]
+        public string PackageJsonPath { get; set; }
+
+        [Required]
+        public string Version { get; set; }
+
+        public override bool Execute()
+        {
+            //System.Diagnostics.Debugger.Launch(); 
+            try
+            {
+                if (!File.Exists(PackageJsonPath))
+                {
+                    return false;
+                }
+                var packageJsonText = File.ReadAllText(PackageJsonPath); 
+                packageJsonText = packageJsonText.Replace(@"""version"": ""1.0.0""", $@"""version"": ""{Version}""");
+                File.WriteAllText(PackageJsonPath, packageJsonText); 
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true; 
+        }
+    }
+}
