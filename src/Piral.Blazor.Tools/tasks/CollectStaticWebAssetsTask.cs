cc4608113371b7650aa99756b1ccb28a61120a00
Lukas Eiter <lukas.eiter@casablanca.at>
2022-02-16 17:10:00 +0100

    collect static web assets from referenced packages

---
+++ src/Piral.Blazor.Tools/tasks/CollectStaticWebAssetsTask.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.IO;
+
+namespace Piral.Blazor.Tools.Tasks
+{
+    public class CollectStaticWebAssetsTask : Task
+    {
+        [Required]
+        public string AssetPath { get; set; }
+
+        [Required]
+        public string TargetPath { get; set; }
+
+        [Required]
+        public string[] ProjectsWithStaticFiles { get; set; }
+
+        public override bool Execute()
+        {
+            // System.Diagnostics.Debugger.Launch(); 
+            try
+            {
+                foreach (string projectName in ProjectsWithStaticFiles)
+                {
+                    if (AssetPath.Contains(projectName))
+                    {
+                        var fileName = Path.GetFileName(AssetPath);
+                        var folderName = $"{TargetPath}/{projectName}";
+
+                        if (!Directory.Exists(folderName))
+                        {
+                            Directory.CreateDirectory(folderName);
+                        }
+
+                        File.Copy(AssetPath, $"{folderName}/{fileName}", true); 
+                        Log.LogMessage($"File '{AssetPath}' copied.");  
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Log.LogError(error.Message);  
+                return false;
+            }
+            return true; 
+        }
+    }
+}
