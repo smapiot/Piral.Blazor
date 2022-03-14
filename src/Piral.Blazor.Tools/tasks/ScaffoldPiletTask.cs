58569bfc7afd1a13e9e7d74a39c9e44aeec384cb
Florian Rappl <mail@florian-rappl.de>
2022-03-14 03:11:31 +0100

    Updated for Windows

--- src/Piral.Blazor.Tools/tasks/ScaffoldPiletTask.cs
+++ src/Piral.Blazor.Tools/tasks/ScaffoldPiletTask.cs
@@ -73,6 +73,10 @@ namespace Piral.Blazor.Tools.Tasks
                 Directory.CreateDirectory(Target);
             }
 
+            var isWindows = Environment.OSVersion.Platform == PlatformID.Win32NT;
+            var cmd = isWindows ? "cmd.exe" : "npx";
+            var prefix = isWindows ? "/c npx.cmd " : "";
+
             try
             {
                 var target = Path.Combine(Target, ProjectName);
@@ -98,7 +102,7 @@ namespace Piral.Blazor.Tools.Tasks
                     Log.LogMessage($"Updating the pilet infrastructure using piral-cli@{CliVersion}...");
 
                     Process
-                        .Start("npx", $"--package=piral-cli@{CliVersion} -y -- pilet upgrade {emulator} --base {target}")
+                        .Start(cmd, $"{prefix}--package=piral-cli@{CliVersion} -y -- pilet upgrade {emulator} --base {target}")
                         .WaitForExit();
                 }
                 else
@@ -113,7 +117,7 @@ namespace Piral.Blazor.Tools.Tasks
                     Directory.CreateDirectory(target);
 
                     Process
-                        .Start("npx", $"--package=piral-cli@{CliVersion} -y -- pilet new {emulator} --base {target} --registry {NpmRegistry} --bundler {Bundler} --no-install")
+                        .Start(cmd, $"{prefix}--package=piral-cli@{CliVersion} -y -- pilet new {emulator} --base {target} --registry {NpmRegistry} --bundler {Bundler} --no-install")
                         .WaitForExit();
                 }
 
