d018a29fa5b6f9ec876fb945dbf0d6bdd66eb033
Florian Rappl <florian.rappl@smapiot.com>
2022-12-02 17:25:57 +0100

    Added setup to template

---
+++ src/Piral.Blazor.Template/content/setup.tsx
@@ -0,0 +1,7 @@
+// import / define the API of your app shell below
+type PiletApi = {};
+type AddScript = (path: string, attrs?: Record<string, string>) => void;
+
+export default function(api: PiletApi, addScript: AddScript) {
+  // wire-up your components here
+}
