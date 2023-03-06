828fb642fa4b14abf2caabe4c850217c4a443e2c
Florian Rappl <mail@florian-rappl.de>
2023-03-06 09:40:31 +0100

    Improved pipeline and added package description

--- src/Piral.Blazor.Tools/tasks/ManagePiletTask.cs
+++ src/Piral.Blazor.Tools/tasks/ManagePiletTask.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Piral.Blazor.Tools.Models;
 using System;
+using System.Reflection;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -91,7 +92,7 @@ namespace Piral.Blazor.Tools.Tasks
 
         private string Emulator => IsPiralInstanceFile ? PiralInstanceFile : PiralInstance;
 
-        private string ToolsVersion => typeof(ManagePiletTask).Assembly.GetName().Version.ToString();
+        private string ToolsVersion => typeof(ManagePiletTask).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
 
         #endregion
 
