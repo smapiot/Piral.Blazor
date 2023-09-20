370934a655ae2a93a47fbb1ce5463d3f9eb74d88
Florian Rappl <mail@florian-rappl.de>
2023-09-20 10:46:08 +0200

    Added some more useful extensions

--- src/Piral.Blazor.Utils/PiletServiceExtensions.cs
+++ src/Piral.Blazor.Utils/PiletServiceExtensions.cs
@@ -18,4 +18,40 @@ public static class PiletServiceExtensions
     {
         return service.Call<string>("getAccessToken");
     }
+
+    /// <summary>
+    /// Gets the currently stored data item using the "getData" pilet API.
+    /// </summary>
+    public static Task<T> GetDataValue<T>(this IPiletService service, string name)
+    {
+        return service.Call<T>("getData", name);
+    }
+
+    /// <summary>
+    /// Sets the stored data item using the "setData" pilet API.
+    /// </summary>
+    public static Task<bool> SetDataValue<T>(this IPiletService service, string name, T value)
+    {
+        return service.Call<bool>("setData", name, value);
+    }
+
+    /// <summary>
+    /// Shows the modal registered with the given name.
+    /// Important: This only works if the "showModal" API is available on the pilet API.
+    /// To make this available you'd need to have the piral-modals plugin installed in your shell.
+    /// </summary>
+    public static async Task ShowModal<T>(this IPiletService service, string name, T options)
+    {
+        await service.Call<object>("showModal", name, options);
+    }
+
+    /// <summary>
+    /// Shows the notification message.
+    /// Important: This only works if the "showNotification" API is available on the pilet API.
+    /// To make this available you'd need to have the piral-notifications plugin installed in your shell.
+    /// </summary>
+    public static async Task ShowNotification<T>(this IPiletService service, string content, T options)
+    {
+        await service.Call<object>("showNotification", content, options);
+    }
 }
