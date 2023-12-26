10798fc0367c6b891ba73bb07544a2c50d10eb8e
Florian Rappl <florian.rappl@smapiot.com>
2023-12-26 02:05:40 +0100

    Improved activator for .NET 8

--- src/Piral.Blazor.Core/PiletComponentActivator.cs
+++ src/Piral.Blazor.Core/PiletComponentActivator.cs
@@ -10,7 +10,8 @@ namespace Piral.Blazor.Core;
 internal class PiletComponentActivator : IComponentActivator
 {
 	private readonly IModuleContainerService _container;
-	private readonly ConcurrentDictionary<Type, Action<IServiceProvider, IComponent>> _cachedInitializers;
+	private readonly object _cachedInitializers;
+	private readonly Type _ctiType;
 	private readonly Func<Type, Action<IServiceProvider, IComponent>> _createInitializer;
 
 	public PiletComponentActivator(IModuleContainerService container)
@@ -18,8 +19,9 @@ internal class PiletComponentActivator : IComponentActivator
 		_container = container;
 
 		var cf = typeof(Renderer).Assembly.GetType("Microsoft.AspNetCore.Components.ComponentFactory")!;
-		_cachedInitializers = (ConcurrentDictionary<Type, Action<IServiceProvider, IComponent>>)cf.GetField("_cachedInitializers", BindingFlags.Static | BindingFlags.NonPublic)!.GetValue(null)!;
-		_createInitializer = cf.GetMethod("CreateInitializer", BindingFlags.NonPublic | BindingFlags.Static)!.CreateDelegate<Func<Type, Action<IServiceProvider, IComponent>>>();
+		_cachedInitializers = cf.GetField("_cachedComponentTypeInfo", BindingFlags.Static | BindingFlags.NonPublic)!.GetValue(null)!;
+		_ctiType = cf.GetNestedType("ComponentTypeInfoCacheEntry", BindingFlags.NonPublic)!;
+		_createInitializer = cf.GetMethod("CreatePropertyInjector", BindingFlags.NonPublic | BindingFlags.Static)!.CreateDelegate<Func<Type, Action<IServiceProvider, IComponent>>>();
 	}
 
 	public IComponent CreateInstance([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type componentType)
@@ -31,7 +33,7 @@ internal class PiletComponentActivator : IComponentActivator
 
 		var child = (IComponent)Activator.CreateInstance(componentType)!;
 
-		if (!_cachedInitializers.ContainsKey(componentType))
+		if (!CacheContainsKey(componentType))
 		{
 			var origin = componentType.Assembly;
 			var provider = _container.GetProvider(origin);
@@ -40,10 +42,22 @@ internal class PiletComponentActivator : IComponentActivator
 			// local DI has been found - use it
 			if (provider is not null)
 			{
-				_cachedInitializers.TryAdd(componentType, (_, cmp) => initializer(provider, cmp));
+				AddInitializerToCache(componentType, provider, initializer);
 			}
 		}
 
 		return child;
+	}
+
+	private bool CacheContainsKey(Type componentType)
+	{
+		return (bool)_cachedInitializers.GetType().GetMethod("ContainsKey").Invoke(_cachedInitializers, new object[]{componentType});
+	}
+
+	private void AddInitializerToCache(Type componentType, IServiceProvider provider, Action<IServiceProvider, IComponent> initializer)
+	{
+		Action<IServiceProvider, IComponent> propInjection = (_, cmp) => initializer(provider, cmp);
+		var cti = Activator.CreateInstance(_ctiType, null, propInjection);
+		_cachedInitializers.GetType().GetMethod("TryAdd").Invoke(_cachedInitializers, new object[]{componentType, cti});
 	}
 }
