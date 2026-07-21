cc372494d9ed1c027d44c568b37e2044d9facf8b
Florian Rappl <florian.rappl@smapiot.com>
2026-07-21 20:04:55 +0200

    Optimize DI for .NET 10

--- src/Piral.Blazor.Core/ModuleContainerService.cs
+++ src/Piral.Blazor.Core/ModuleContainerService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Piral.Blazor.Utils;
 using System;
@@ -11,12 +12,21 @@ namespace Piral.Blazor.Core;
 
 public class ModuleContainerService : IModuleContainerService
 {
+    private static readonly ICacheManipulatorService NoopCacheManipulator = new NoopCacheManipulatorService();
+
     private readonly IPiralServiceProvider _provider;
+    private readonly ICacheManipulatorService _cacheManipulator;
     private readonly Dictionary<AssemblyLoadContext, IServiceProvider> _providers = new();
 
     public ModuleContainerService(IPiralServiceProvider provider)
+        : this(provider, NoopCacheManipulator)
+    {
+    }
+
+    public ModuleContainerService(IPiralServiceProvider provider, ICacheManipulatorService cacheManipulator)
     {
         _provider = provider;
+        _cacheManipulator = cacheManipulator ?? NoopCacheManipulator;
         _providers.Add(AssemblyLoadContext.Default, _provider);
     }
 
@@ -29,7 +39,9 @@ public class ModuleContainerService : IModuleContainerService
         ConfigureLocalServices(services, assembly, pilet);
         ConfigureDefaultServices(services, assembly, pilet);
 
-        return _providers[alc] = CreateProvider(alc, services);
+        var provider = CreateProvider(alc, services);
+        PrewarmComponentCache(assembly, provider);
+        return _providers[alc] = provider;
     }
 
     private IServiceProvider CreateProvider(AssemblyLoadContext alc, ServiceCollection services)
@@ -89,5 +101,20 @@ public class ModuleContainerService : IModuleContainerService
             .GetTypes()
             .FirstOrDefault(x => string.Equals(x.Name, "Module", StringComparison.Ordinal))
             ?.GetMethod(name, BindingFlags.Public | BindingFlags.Static, null, parameters, null);
+    }
+
+    private void PrewarmComponentCache(Assembly assembly, IServiceProvider provider)
+    {
+        foreach (var componentType in assembly.GetTypes().Where(m => !m.IsAbstract && typeof(IComponent).IsAssignableFrom(m)))
+        {
+            _cacheManipulator.UpdateComponentCache(componentType, provider);
+        }
+    }
+
+    private sealed class NoopCacheManipulatorService : ICacheManipulatorService
+    {
+        public void UpdateComponentCache(Type componentType, IServiceProvider provider)
+        {
+        }
     }
 }
